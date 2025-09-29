83823c2b66af28438d117da65cb183adb3631176
marouane <marouane@gmail.com>
2025-09-29 03:08:46 +0100

    check sharukhana

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,7 @@ route.post("/create",create)
 route.get("/fetch",fetch)
 route.put("/update/:id",update)
 route.delete("/delete/:id",deleteClient)
+// route.delete("/delete/:id",deleteClient)
 
 
 export default route;
